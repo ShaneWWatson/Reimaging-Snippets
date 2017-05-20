1cd30d203118f96b8676a2b176515eaf2711ddb2
Shane Watson <technician2all@gmail.com>
2017-05-20 08:56:38 -0500

    Add files via upload

---
+++ RemoveUselessWin10.bat
@@ -0,0 +1,7 @@
+PowerShell -ExecutionPolicy Unrestricted -Command "Get-AppxPackage *3dbuilder* | Remove-AppxPackage"
+PowerShell -ExecutionPolicy Unrestricted -Command "Get-AppxPackage *officehub* | Remove-AppxPackage"
+PowerShell -ExecutionPolicy Unrestricted -Command "Get-AppxPackage *skypeapp* | Remove-AppxPackage"
+PowerShell -ExecutionPolicy Unrestricted -Command "Get-AppxPackage *getstarted* | Remove-AppxPackage"
+PowerShell -ExecutionPolicy Unrestricted -Command "Get-AppxPackage *solitairecollection* | Remove-AppxPackage"
+PowerShell -ExecutionPolicy Unrestricted -Command "Get-AppxPackage *windowsphone* | Remove-AppxPackage"
+PowerShell -ExecutionPolicy Unrestricted -Command "Get-AppxPackage *xboxapp* | Remove-AppxPackage"
